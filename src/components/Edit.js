45d42f0b95f5cf0eb5af96d690bb57a0e56f837f
Alex <alexadecimal@icloud.com>
2019-02-27 21:23:33 -0600

    Actualización, pendiente de eliminar post a actualizar.

--- src/components/Edit.js
+++ src/components/Edit.js
@@ -4,13 +4,16 @@ import { connect } from 'react-redux'
 import *  as actions from '../redux/actions/index' /* el * se lee como "todo" */
 
 class Edit extends Component {
+
   componentDidMount() {
     let {
       posts, match, updateTitle, updateExcerpt, 
       updateContent, updateAuthor
     } = this.props
 
-    let result = posts.find(post => post.created === parseInt(match.params.id ));
+    console.log(match.params.id)//Aqui se imprime el ID sin problema
+
+    let result = posts.find(post => post.created === parseInt(match.params.id ));//esto localiza el id del post en cuestion
     updateTitle(result.title)
     updateExcerpt(result.excerpt)
     updateContent(result.content)
@@ -20,63 +23,64 @@ class Edit extends Component {
   render() {
     let {
       title, excerpt, content, 
-      author, updateTitle, updateExcerpt, 
+      author, created, updateTitle, updateExcerpt, 
       updateContent, updateAuthor, saveUpdate,
    } = this.props
   
     return (
       <div className="row justify-content-center my-5">
-      <div className="col-8">
-        <div className = "card bg-light">
-          <div className="card-body">
-            <div className ="card-body">
-              <h3 className= "card-title text-center">New Post</h3>
-
-              <div className = "form-group">
-                <input 
-                className="form-control" 
-                value = {title}
-                onChange = {e => updateTitle(e.target.value)} />
-              </div>
+        <div className="col-8">
+          <div className = "card bg-light">
+            <div className="card-body">
+              <div className ="card-body">
+                <h3 className= "card-title text-center">New Post</h3>
+                <div className = "form-group">
+                  <input 
+                  className="form-control" 
+                  value = {title}
+                  onChange = {e => updateTitle(e.target.value)} />
+                </div>
               
-              <div className = "form-group">
-                <textarea 
-                className="form-control" 
-                col="3"
-                value = {excerpt}
-                onChange = {e => updateExcerpt(e.target.value)} ></textarea>
-              </div>
+                <div className = "form-group">
+                  <textarea 
+                  className="form-control" 
+                  col="3"
+                  value = {excerpt}
+                  onChange = {e => updateExcerpt(e.target.value)} ></textarea>
+                </div>
 
-              <div className = "form-group">
-                <textarea 
-                className="form-control" 
-                col="3" 
-                value = {content}
-                onChange = {e => updateContent(e.target.value)}></textarea>
-              </div>
+                <div className = "form-group">
+                  <textarea 
+                  className="form-control" 
+                  col="3" 
+                  value = {content}
+                  onChange = {e => updateContent(e.target.value)}></textarea>
+                </div>
 
-              <div className = "form-group">
-                <input className = "form-control"
-                value = {author}
-                onChange = {e => updateAuthor(e.target.value)}/>
-              </div>
+                <div className = "form-group">
+                  <input className = "form-control"
+                  value = {author}
+                  onChange = {e => updateAuthor(e.target.value)}/>
+                </div>
 
-              <button className = "btn btn-primary"
-              onClick = {() => saveUpdate()}> Update </button>
-              <Link to = "/home" className = "btn btn-secondary"> Cancel </Link>
+                <div>{this.props.match.params.id}  </div>
+
+                <button className = "btn btn-primary"
+                  onClick = {() => saveUpdate()}> Update </button>
+                <Link to = "/home" className = "btn btn-secondary"> Cancel </Link>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
     )}
   }
 
 
 
 const mapStateToProps = ({post}) => {
-  const { title, excerpt, content, author } = post.post
+  const { title, excerpt, content, author, created } = post.post
   const { posts } = post
-  return { title, excerpt, content, author, posts }
+  return { title, excerpt, content, author, posts, created }
 }
 export default connect(mapStateToProps, actions) (Edit) /* currying closures */
--- src/redux/reducers/postReducer.js
+++ src/redux/reducers/postReducer.js
@@ -17,15 +17,22 @@ export default (state = INITIAL_STATE, action)=>{ /* Esto solo va a asignar el v
   switch (action.type) { 
 
     case SAVE_UPDATE :
+      //crear un ciclo for que compare el key y cuente para calcular el indice, al cual le haremos pop
+      
       post.created = Date.now()
-      posts.splice( 0 , 1 , post )
-      //posts.push(post) //hACE FALTA ENCARGARME DE QUE SE REEMPLACE EL POST, NO sE GUARDE UNO NUEVO
-      post = {
-        title:'',
-        excerpt:'',
-        content:'',
-        author:'', //Esto lo que hace es limpiar los campos de los inputs.
-      }
+        if (post.title.length && post.title && post.excerpt.length && post.excerpt && post.content.length && post.content && post.author.length && post.author){
+          
+          posts.push( post )
+          //posts.push(post) //hACE FALTA ENCARGARME DE QUE SE REEMPLACE EL POST, NO sE GUARDE UNO NUEVO
+          post = {
+            title:'',
+            excerpt:'',
+            content:'',
+            author:'', //Esto lo que hace es limpiar los campos de los inputs.
+          }
+
+        }
+        
       return { ...state, post }
       
     case UPDATE_TITLE :
@@ -45,14 +52,16 @@ export default (state = INITIAL_STATE, action)=>{ /* Esto solo va a asignar el v
       return { ...state, post }  
 
     case UPDATE_POSTS :
-      post.created = Date.now()         //aqui se tiene que declarar el created, antes del push.
-        posts.push(post)
-        post = {
-          title:'',
-          excerpt:'',
-          content:'',
-          author:'',
-          created:'',                      //Esto lo que hace es limpiar los campos de los inputs.
+      if (post.title.length && post.title && post.excerpt.length && post.excerpt && post.content.length && post.content && post.author.length && post.author){
+        post.created = Date.now()         //aqui se tiene que declarar el created, antes del push.
+          posts.push(post)
+            post = {
+            title:'',
+            excerpt:'',
+            content:'',
+            author:'',
+            created:'',                      //Esto lo que hace es limpiar los campos de los inputs.
+          }
         }//Aqui no se puede declarar el created por que esto limpia el estado.
       return {...state, post, posts}
     default:
